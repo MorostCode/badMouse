c07eb3c20f0bba26b639b4726999204974826e85
Morost <42142557+MorostCode@users.noreply.github.com>
2022-10-20 10:38:52 +0800

    更新gitignore

--- test.py
+++ test.py
@@ -13,10 +13,4 @@ def image_to_base64(path):
         # 返回base64编码字符串
         return b64_encode
 
-
 # print(image_to_base64("badMouse.png"))
-
-url = "https://space.bilibili.com/8020359?spm_id_from=444.41.0.0"
-url2 = "https://space.bilibili.com/473500883/video?tid=0&page=2&keyword=&order=pubdate"
-url3 = "https://www.douyin.com/user/MS4wLjABAAAAUwyh5pntLvnqzRRO5j9Y9etaoYmTf0O3R0y3tpLB4nBElLNMbDY-0zERKF2p0o-2?vid=7134558532518579471"
-print(re.search('\d+$', url3).group())
